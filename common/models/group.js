f23444f6c196af8ced706a435d4cf7932bfe3fa2
Roel Van Zeebroeck <rvz@dwesam.com>
2018-09-19 19:44:23 +0200

    Use auth0 authentication

--- common/models/group.js
+++ common/models/group.js
@@ -3,7 +3,18 @@
 module.exports = function(Group) {
   Group.beforeRemote('create', function(context, user, next) {
     context.args.data.creationDate = Date.now();
-    context.args.data.creatorId = context.req.accessToken.userId;
+    // context.args.data.creatorId = context.req.accessToken.userId;
+    next();
+  });
+};
+
+module.exports = function(Group) {
+  Group.beforeRemote('**', function(context, user, next) {
+    // TODO: check in correct way
+    if (context.methodString === 'Group.prototype.__create__messages') {
+      context.args.data.creationDate = Date.now();
+      console.log(user);
+    }
     next();
   });
 };
--- server/server.js
+++ server/server.js
@@ -2,8 +2,41 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 
 var app = module.exports = loopback();
+
+var authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    // YOUR-AUTH0-DOMAIN name e.g https://prosper.auth0.com
+    jwksUri: 'https://chalendar.eu.auth0.com/.well-known/jwks.json',
+  }),
+  // This is the identifier we set when we created the API
+  audience: 'https://chalendar.com/',
+  // issuer: 'https://chalendar.eu.auth0.com',
+  algorithms: ['RS256'],
+});
+
+app.use(authCheck);
+
+// apply to a path
+// app.use('/api/Groups', function(req, res, next) {
+//   res.json('It has valid token', req.user);
+// });
+
+// catch error
+app.use(function(err, req, res, next) {
+  console.log(err);
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Invalid token, or no token supplied!');
+  } else {
+    res.status(401).send('Unexpected error');
+  }
+});
 
 app.start = function() {
   // start the web server
