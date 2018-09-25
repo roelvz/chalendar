ca74d49906277b578bfd1aa2d89b41274029d572
Roel Van Zeebroeck <rvz@dwesam.com>
2018-09-25 20:19:05 +0200

    Set message properties for event messages onCreate

--- common/models/event.js
+++ common/models/event.js
@@ -7,3 +7,15 @@ module.exports = function(Event) {
     next();
   });
 };
+
+module.exports = function(Event) {
+  Event.beforeRemote('**', function(context, user, next) {
+    console.log(context.methodString);
+    // TODO: check in correct way
+    if (context.methodString === 'Event.prototype.__create__messages') {
+      context.args.data.creationDate = Date.now();
+      context.args.data.creatorId = context.req.user.sub;
+    }
+    next();
+  });
+};
