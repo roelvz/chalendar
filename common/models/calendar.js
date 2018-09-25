b4d239ca96ec2187af29679257f6257ec19c07e9
Roel Van Zeebroeck <rvz@dwesam.com>
2018-09-25 19:43:45 +0200

    Fill in creator id's of entities

--- common/models/calendar.js
+++ common/models/calendar.js
@@ -3,7 +3,7 @@
 module.exports = function(Calendar) {
   Calendar.beforeRemote('create', function(context, user, next) {
     context.args.data.creationDate = Date.now();
-    context.args.data.creatorId = context.req.accessToken.userId;
+    context.args.data.creatorId = context.req.user.sub;
     next();
   });
 };
--- common/models/group.js
+++ common/models/group.js
@@ -3,7 +3,7 @@
 module.exports = function(Group) {
   Group.beforeRemote('create', function(context, user, next) {
     context.args.data.creationDate = Date.now();
-    // context.args.data.creatorId = context.req.accessToken.userId;
+    context.args.data.creatorId = context.req.user.sub;
     next();
   });
 };
@@ -13,7 +13,7 @@ module.exports = function(Group) {
     // TODO: check in correct way
     if (context.methodString === 'Group.prototype.__create__messages') {
       context.args.data.creationDate = Date.now();
-      console.log(context.req.user);
+      context.args.data.creatorId = context.req.user.sub;
     }
     next();
   });
