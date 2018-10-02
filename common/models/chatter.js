10101981529659c85aa212f0882a99505b3dfb49
Roel Van Zeebroeck <rvz@dwesam.com>
2018-10-02 21:28:30 +0200

    Return unread messages for events

--- common/models/chatter.js
+++ common/models/chatter.js
@@ -4,7 +4,7 @@ module.exports = function(Chatter) {
   Chatter.afterRemote('**', function(context, user, next) {
     let chatModel = Chatter.app.models.Chat;
     let chatterInChatModel = Chatter.app.models.ChatterInChat;
-    let evenModel = Chatter.app.models.Event;
+    let eventModel = Chatter.app.models.Event;
 
     if (context.methodString === 'Chatter.prototype.__get__groups') {
       // Calculate # new messages for group
@@ -36,33 +36,39 @@ module.exports = function(Chatter) {
       // Calculate # new messages for calendar
       let chatterId = context.instance.id;
       let promises = [];
+      let promises2 = [];
       for (let i = 0; i < context.result.length; i++) {
         let calendar = context.result[i];
         calendar.newMessages = 0;
 
-        promises.push(evenModel.findOne({where: {
+        promises.push(eventModel.find({where: {
           calendarId: calendar.id,
         }})
-          .then(event => {
-            return chatModel.findOne({where: {
-              eventId: event.id,
-            }});
-          })
-          .then(chat => {
-            return chatterInChatModel.findOne({
-              where: {
-                chatId: chat.id,
-                chatterId: chatterId,
-              },
-            });
-          })
-          .then(chatterInChat => {
-            calendar.newMessages +=
-              chatterInChat ? chatterInChat.newMessages : 0;
+          .then(events => {
+            for (let j = 0; j < events.length; j++) {
+              let event = events[j];
+
+              promises2.push(chatModel.findOne({where: {
+                eventId: event.id,
+              }})
+                .then(chat => {
+                  return chatterInChatModel.findOne({
+                    where: {
+                      chatId: chat.id,
+                      chatterId: chatterId,
+                    },
+                  });
+                })
+                .then(chatterInChat => {
+                  calendar.newMessages +=
+                    chatterInChat ? chatterInChat.newMessages : 0;
+                }));
+            }
           })
         );
       }
       Promise.all(promises)
+        .then(result => Promise.all(promises2))
         .then(result => next())
         .catch(error => next());
     } else {
