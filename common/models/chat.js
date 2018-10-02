dee5bde5afd956e6598b42ce4b26c0f6212b7483
Roel Van Zeebroeck <rvz@dwesam.com>
2018-10-02 21:03:27 +0200

    Keep number of unread messages

--- common/models/chat.js
+++ common/models/chat.js
@@ -1,18 +1,86 @@
 'use strict';
 
 module.exports = function(Chat) {
-};
-
-module.exports = function(Chat) {
+  // Set creatorId and creationDate for a new message.
   Chat.beforeRemote('**', function(context, user, next) {
-    // TODO: check in correct way
     if (context.methodString === 'Chat.prototype.__create__messages') {
-      context.args.data.creationDate = Date.now();
+      let creatorId = 'DEV_USER';
       if (context.req && context.req.user && context.req.user.sub) {
-        context.args.data.creatorId = context.req.user.sub;
-      } else {
-        context.args.data.creatorId = 'DEV_USER';
+        creatorId = context.req.user.sub;
       }
+      context.args.data.creatorId = creatorId;
+      context.args.data.creationDate = Date.now();
+    }
+    next();
+  });
+
+  // Set number of new messages to 0 when reading a chat's messages
+  Chat.afterRemote('**', function(context, user, next) {
+    if (context.methodString === 'Chat.prototype.__get__messages') {
+      let chatterInChat = Chat.app.models.ChatterInChat;
+      let creatorId = 'DEV_USER';
+      if (context.req && context.req.user && context.req.user.sub) {
+        creatorId = context.req.user.sub;
+      }
+
+      let chatId = context.instance.id;
+
+      chatterInChat.findOne({where: {
+        chatId: chatId,
+        chatterId: creatorId,
+      }})
+        .then(result => {
+          if (result) {
+            chatterInChat.upsert({
+              id: result.id,
+              chatId: chatId,
+              chatterId: creatorId,
+              newMessages: 0,
+            });
+          }
+        });
+    }
+    next();
+  });
+
+  // Increase number of new messages when adding a message
+  Chat.afterRemote('**', function(context, user, next) {
+    if (context.methodString === 'Chat.prototype.__create__messages') {
+      let chatterInChat = Chat.app.models.ChatterInChat;
+
+      let creatorId = 'DEV_USER';
+      if (context.req && context.req.user && context.req.user.sub) {
+        creatorId = context.req.user.sub;
+      }
+
+      let chatId = context.instance.id;
+
+      Chat.app.models.Chatter.find()
+        .then(chatters => {
+          let promises = [];
+
+          // Increase new message count for all other users
+          // TODO: only for members of group/calendar
+          for (let i = 0; i < chatters.length; i++) {
+            let chatter = chatters[i];
+
+            // No need to increase new message count for creator of message
+            if (chatter.id !== creatorId) {
+              chatterInChat.findOne({where: {
+                chatId: chatId,
+                chatterId: chatter.id,
+              }})
+                .then(result => {
+                  chatterInChat.upsert({
+                    id: result ? result.id : undefined,
+                    chatId: chatId,
+                    chatterId: chatter.id,
+                    newMessages: result ? result.newMessages + 1 : 1,
+                  });
+                });
+            }
+          }
+        });
     }
     next();
   });
--- common/models/chatter.js
+++ common/models/chatter.js
@@ -1,5 +1,39 @@
 'use strict';
 
 module.exports = function(Chatter) {
+  Chatter.afterRemote('**', function(context, user, next) {
+    if (context.methodString === 'Chatter.prototype.__get__groups') {
+      let chatterId = context.instance.id;
 
+      let chatModel = Chatter.app.models.Chat;
+      let chatterInChatModel = Chatter.app.models.ChatterInChat;
+
+      let promises = [];
+      for (let i = 0; i < context.result.length; i++) {
+        let group = context.result[i];
+
+        promises.push(chatModel.findOne({where: {
+          groupId: group.id,
+        }})
+          .then(chat => {
+            return chatterInChatModel.findOne({
+              where: {
+                chatId: chat.id,
+                chatterId: chatterId,
+              },
+            });
+          })
+          .then(chatterInChat => {
+            group.newMessages =
+              chatterInChat ? chatterInChat.newMessages : 0;
+          }));
+      }
+
+      Promise.all(promises)
+        .then(result => next())
+        .catch(error => next());
+    } else {
+      next();
+    }
+  });
 };
