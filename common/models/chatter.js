7600cb6b538be52507296fdeb80cddbc76539487
Roel Van Zeebroeck <rvz@dwesam.com>
2018-10-02 21:22:27 +0200

    Return number of unread messages for calendars

--- common/models/chatter.js
+++ common/models/chatter.js
@@ -2,12 +2,13 @@
 
 module.exports = function(Chatter) {
   Chatter.afterRemote('**', function(context, user, next) {
+    let chatModel = Chatter.app.models.Chat;
+    let chatterInChatModel = Chatter.app.models.ChatterInChat;
+    let evenModel = Chatter.app.models.Event;
+
     if (context.methodString === 'Chatter.prototype.__get__groups') {
+      // Calculate # new messages for group
       let chatterId = context.instance.id;
-
-      let chatModel = Chatter.app.models.Chat;
-      let chatterInChatModel = Chatter.app.models.ChatterInChat;
-
       let promises = [];
       for (let i = 0; i < context.result.length; i++) {
         let group = context.result[i];
@@ -28,7 +29,39 @@ module.exports = function(Chatter) {
               chatterInChat ? chatterInChat.newMessages : 0;
           }));
       }
+      Promise.all(promises)
+        .then(result => next())
+        .catch(error => next());
+    } else if (context.methodString === 'Chatter.prototype.__get__calendars') {
+      // Calculate # new messages for calendar
+      let chatterId = context.instance.id;
+      let promises = [];
+      for (let i = 0; i < context.result.length; i++) {
+        let calendar = context.result[i];
+        calendar.newMessages = 0;
 
+        promises.push(evenModel.findOne({where: {
+          calendarId: calendar.id,
+        }})
+          .then(event => {
+            return chatModel.findOne({where: {
+              eventId: event.id,
+            }});
+          })
+          .then(chat => {
+            return chatterInChatModel.findOne({
+              where: {
+                chatId: chat.id,
+                chatterId: chatterId,
+              },
+            });
+          })
+          .then(chatterInChat => {
+            calendar.newMessages +=
+              chatterInChat ? chatterInChat.newMessages : 0;
+          })
+        );
+      }
       Promise.all(promises)
         .then(result => next())
         .catch(error => next());
