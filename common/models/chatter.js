0cccfe9348d155f4a3079873db396ec5b9ec0a60
Roel Van Zeebroeck <rvz@dwesam.com>
2018-11-06 19:31:47 +0100

    Improved message count calculation for calendars

--- common/models/chatter.js
+++ common/models/chatter.js
@@ -2,70 +2,56 @@
 
 module.exports = function(Chatter) {
   Chatter.afterRemote('**', function(context, user, next) {
-    let chatModel = Chatter.app.models.Chat;
     let chatterInChatModel = Chatter.app.models.ChatterInChat;
-    let eventModel = Chatter.app.models.Event;
 
+    let promises = [];
     if (context.methodString === 'Chatter.prototype.__get__groups') {
       // Calculate # new messages for group
       let chatterId = context.instance.id;
-      let promises = [];
+
       for (let i = 0; i < context.result.length; i++) {
         let group = context.result[i];
+        let chat = group.chat();
 
-        chatterInChatModel.findOne({
+        promises.push(chatterInChatModel.findOne({
           where: {
-            chatId: group.chat.id,
+            chatId: chat.id,
             chatterId: chatterId,
           },
         }).then(chatterInChat => {
           group.newMessages = chatterInChat ? chatterInChat.newMessages : 0;
-        });
+        }));
       }
-      Promise.all(promises)
-        .then(result => next())
-        .catch(error => next());
     } else if (context.methodString === 'Chatter.prototype.__get__calendars') {
       // Calculate # new messages for calendar
       let chatterId = context.instance.id;
-      let promises = [];
-      let promises2 = [];
+
       for (let i = 0; i < context.result.length; i++) {
         let calendar = context.result[i];
         calendar.newMessages = 0;
+        let events = calendar.events();
 
-        promises.push(eventModel.find({where: {
-          calendarId: calendar.id,
-        }})
-          .then(events => {
-            for (let j = 0; j < events.length; j++) {
-              let event = events[j];
+        for (let j = 0; j < events.length; j++) {
+          let event = events[j];
+          let chat = event.chat();
 
-              promises2.push(chatModel.findOne({where: {
-                eventId: event.id,
-              }})
-                .then(chat => {
-                  return chatterInChatModel.findOne({
-                    where: {
-                      chatId: chat.id,
-                      chatterId: chatterId,
-                    },
-                  });
-                })
-                .then(chatterInChat => {
-                  calendar.newMessages +=
-                    chatterInChat ? chatterInChat.newMessages : 0;
-                }));
-            }
-          })
-        );
+          promises.push(chatterInChatModel.findOne({
+            where: {
+              chatId: chat.id,
+              chatterId: chatterId,
+            },
+          }).then(chatterInChat => {
+            calendar.newMessages +=
+              chatterInChat ? chatterInChat.newMessages : 0;
+          }));
+        }
       }
-      Promise.all(promises)
-        .then(result => Promise.all(promises2))
-        .then(result => next())
-        .catch(error => next());
-    } else {
-      next();
     }
+    Promise.all(promises)
+      .then(() => next())
+      .catch(error => {
+        console.error(error);
+        next();
+      });
   });
 };
