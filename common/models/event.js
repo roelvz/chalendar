25d0a58812d329af69647de63ab620ba0984ae05
Maarten De Brandt <maartendb@gmail.com>
2018-11-25 22:01:51 +0100

    Event creationDate & creatorId
    @roelvz Dat werd nog niet ingevuld in de backend. Heb dit toegevoegd maar lijkt nog steeds niet te werken

--- common/models/event.js
+++ common/models/event.js
@@ -2,6 +2,18 @@
 let utils = require('../../utils/utils.js')();
 
 module.exports = function(Event) {
+  // Set creationDate and creatorId for a new event.
+  // @Roel, dit werkt precies niet? CreationDate en cratorId blijft leeg. Een idee waarom? want da lijkt correct.
+  Event.beforeRemote('create', function(context, user, next) {
+    context.args.data.creationDate = Date.now();
+    if (context.req && context.req.user && context.req.user.sub) {
+      context.args.data.creatorId = context.req.user.sub;
+    } else {
+      context.args.data.creatorId = 'DEV_USER';
+    }
+    next();
+  });
+
   // Set number of new messages to 0 when reading a group's messages
   Event.afterRemote('findById', function(context, user, next) {
     utils.initMessages(Event, user, context);
